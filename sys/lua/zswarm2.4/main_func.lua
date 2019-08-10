a6e2065c00d9a357071fc62fcb80b3d3d87580ae
Berdan Can Yetkiner <13546853+berdancanyet@users.noreply.github.com>
2019-08-10 17:40:00 +0300

    Update and rename zs_main_functions.lua to main_func.lua

---
+++ sys/lua/zswarm2.4/main_func.lua
@@ -0,0 +1,47 @@
+## ZS 2.4 Main Functions
+--[[Do not edit these functions.]]--
+local exec = parse
+
+function _drop(id)
+if player(id,"team") == 1 then return 1;end;end
+
+function _walkover(id,iid,type)
+if player(id,"team")==2 and (type==59) or (type==84) then return 1;
+elseif player(id,"team")==1 and (type~=59) then return 1;end;end
+--[[addhook("collect","nocollect")
+function nocollect(id,iid,type)
+if player(id,"team")==1 and (type~=59) then
+exec("strip "..id.." "..type)
+end
+end ]]--
+function _die(id)
+if player(id,"team")==1 then 
+exec('hudtxt2 '..id..' 1 " " 20 430') return 1;end;end
+
+function _flash(id)
+if player(id,"team")==1 then msg2(id,"Humans can notice you easier, if you use flashlight!@C");end;end
+
+function _buy(id)
+return 1;end
+
+--Callback Functions--
+function equip(id,weapon)
+	exec("equip "..id.." "..weapon) end
+
+function strip(id,weapon)
+	exec("strip "..id.." "..weapon) end
+
+function setmaxhealth(id,maxhealth)
+	exec("setmaxhealth "..id.." "..maxhealth) end
+
+function sethealth(id,health)
+	exec("sethealth "..id.." "..health) end
+
+function speed(id,amount)
+	exec("speedmod "..id.." "..amount) end
+	
+addhook("drop","_drop");
+addhook("walkover","_walkover");
+addhook("die","_die");
+addhook("flashlight","_flash");
+addhook("buy","_buy");
