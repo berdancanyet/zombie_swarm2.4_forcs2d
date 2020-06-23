f5373be057c24951a5bd02639cf3e9dbea512bcd
Berdan Can Yetkiner <13546853+berdancanyet@users.noreply.github.com>
2020-06-23 16:11:57 +0300

    Update lightning_sys.lua

--- sys/lua/zswarm2.4/lightning_sys.lua
+++ sys/lua/zswarm2.4/lightning_sys.lua
@@ -4,20 +4,21 @@ local t = timer
 
 addhook("startround","round")
 function round()
-play("sv_sound zombie_swarm2.4/3dmstart.wav")
-end;round()
+	play("sv_sound zombie_swarm2.4/3dmstart.wav")
+end
+round()
 		
 function thunder()
-		play("sv_sound env/thunder.wav")
-		t(500,"parse",'sv_daylighttime 185')
-		t(500,"parse",'flashplayer 0 40')
-		t(1500,"parse",'sv_daylighttime 21')
+	play("sv_sound env/thunder.wav")
+	t(500,"parse",'sv_daylighttime 185')
+	t(500,"parse",'flashplayer 0 40')
+	t(1500,"parse",'sv_daylighttime 21')
 end
 
 t(15000,"thunder","",0)
 		
 function sound()
-play("sv_sound zombie_swarm2.4/ambience.ogg")
-t(17000,"sound")
+	play("sv_sound zombie_swarm2.4/ambience.ogg")
+	t(17000,"sound")
 end
 sound()
